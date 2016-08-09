4c4dbdea8993fc52c4ed0b2d9bd6f29b23d33d7d
ivanovichivanosky <yoshi.vyc@gmail.com>
2016-08-09 19:06:46 +0800

    Welcome Form Module

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+class App extends Component {
+  state = {
+    name:""
+  };
+  utrohon = (event)=>{
+    var inputName = event.target.value;
+    
+    this.setState({
+      name: inputName
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React, {this.state.name}</h2>
+        </div>
+        <p className="App-intro">
+          <form>
+          Enter your name:&nbsp;
+          <input type="text" onChange={this.utrohon}/>
+          </form>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
